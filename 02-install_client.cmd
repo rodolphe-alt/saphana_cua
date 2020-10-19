3a9ef312984a5d9753fea626c667bb9e8f6d72db
rodolphedj <rodolphedj@gmail.com>
2020-10-19 09:18:00 +0200

    Update 02-install_client.cmd

--- 02-install_client.cmd
+++ 02-install_client.cmd
@@ -4,4 +4,4 @@ zypper install python-pip python3
 pip install pyhdb
 pip install hurry.filesize
 pip install bitmath
--- now edit the py file to add all settings to connect as the DB
+-- now edit the provi_poc_secu.py file to adapt all settings to connect as the DB successfully
