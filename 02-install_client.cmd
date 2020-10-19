17bd721fd0992193967fcd9a5e1cc39926f00abb
rodolphedj <rodolphedj@gmail.com>
2020-10-19 09:10:44 +0200

    Update 02-install_client.cmd

--- 02-install_client.cmd
+++ 02-install_client.cmd
@@ -1,5 +1,7 @@
 -- as OS admin account (root)
 -- here zypper is for SLES
-zypper install python-pip
+zypper install python-pip python3
 pip install pyhdb
 pip install hurry.filesize
+pip install bitmath
+-- now edit the py file to add all settings to connect as the DB
